a7a78334e92002ff26b0ee97b154d3c347775dc6
Aakash Yadav <aakashsyadav1999@gmail.com>
2026-01-10 14:57:42 +0530

    Added static data for backend

---
+++ ai_backend/app/static/static_data.py
@@ -0,0 +1,161 @@
+WARD_DATA = [
+  {
+    "ward_no": "002",
+    "ward_name": "Dariapur",
+    "ward_address": "Dariapur Ward Office, Nr. Kuti Masjid, Nr. Rupapari Ni pol, Dariapur Darawaja, Ahmedabad",
+    "ward_admin_name": "Vinod Patel",
+    "ward_admin_no": "9979605703",
+    "ward_timing": "9.00 am to 4.30 pm",
+    "ward_zone": "central_zone"
+  },
+  {
+    "ward_no": "018",
+    "ward_name": "Nikol",
+    "ward_address": "Nikol Ward Office, Jinal Bunglows, D-Mart Road, Nikol, Ahmedabad",
+    "ward_admin_name": "Jay Zalavadiya",
+    "ward_admin_no": "9274395059",
+    "ward_timing": "9.00 am to 4.30 pm",
+    "ward_zone": "east_zone"
+  },
+  {
+    "ward_no": "026",
+    "ward_name": "Gota",
+    "ward_address": "Gota Ward Office, Nr.ADB Water Tank, Science City Road, Sola, Ahmedabad",
+    "ward_admin_name": "Jaydeep Pandya",
+    "ward_admin_no": "9726415802",
+    "ward_timing": "9.00 am to 4.30 pm",
+    "ward_zone": "north_west_zone"
+  },
+  {
+    "ward_no": "051",
+    "ward_name": "Vatva",
+    "ward_address": "Vatva Ward Office, Beside Kashiba hospital,Vatva Gam, Ahmedabad",
+    "ward_admin_name": "Pritesh Chaudhari",
+    "ward_admin_no": "9274395056",
+    "ward_timing": "9.00 am to 4.30 pm",
+    "ward_zone": "south_zone"
+  }
+]
+
+CONTRACTOR_DATA = [
+  {
+    "ward_no": "002",
+    "contractors": [
+      { "contractor_name": "Jaimin Shah",
+       "mobile_number": "9000000201",
+        "jobs_pending": 3,
+        "rating": 4 
+      },
+      { "contractor_name": "Rahul Mehta",
+       "mobile_number": "9000000202",
+        "jobs_pending": 6,
+        "rating": 5 
+      },
+      { "contractor_name": "Suresh Patel",
+       "mobile_number": "9000000203",
+        "jobs_pending": 2,
+        "rating": 3 
+      },
+      { "contractor_name": "Nitin Joshi",
+       "mobile_number": "9000000204",
+        "jobs_pending": 8,
+        "rating": 4 
+      },
+      { "contractor_name": "Kunal Desai",
+       "mobile_number": "9000000205",
+        "jobs_pending": 1,
+        "rating": 5 
+      }
+    ]
+  },
+  {
+    "ward_no": "018",
+    "contractors": [
+      { "contractor_name": "Vishal Parmar",
+       "mobile_number": "9000001801",
+        "jobs_pending": 4,
+         "rating": 4 
+      },
+      { "contractor_name": "Rakesh Solanki",
+       "mobile_number": "9000001802",
+        "jobs_pending": 7,
+         "rating": 3 
+      },
+      { "contractor_name": "Ankit Verma",
+       "mobile_number": "9000001803",
+        "jobs_pending": 2,
+         "rating": 5 
+      },
+      { "contractor_name": "Manoj Chauhan",
+       "mobile_number": "9000001804",
+        "jobs_pending": 5,
+         "rating": 4 
+      },
+      { "contractor_name": "Deepak Yadav",
+       "mobile_number": "9000001805",
+        "jobs_pending": 9,
+         "rating": 2 
+      }
+    ]
+  },
+  {
+    "ward_no": "026",
+    "contractors": [
+      { "contractor_name": "Harshil Pandya",
+       "mobile_number": "9000002601",
+        "jobs_pending": 6,
+        "rating": 4 
+      },
+      { "contractor_name": "Jayesh Trivedi",
+       "mobile_number": "9000002602",
+        "jobs_pending": 3,
+        "rating": 5 
+      },
+      { "contractor_name": "Paresh Modi",
+       "mobile_number": "9000002603",
+        "jobs_pending": 8,
+        "rating": 3 
+      },
+      { "contractor_name": "Bhavesh Patel",
+       "mobile_number": "9000002604",
+        "jobs_pending": 1,
+        "rating": 5 
+      },
+      { "contractor_name": "Sanjay Thakkar",
+       "mobile_number": "9000002605",
+        "jobs_pending": 4,
+        "rating": 4 
+      }
+    ]
+  },
+  {
+    "ward_no": "051",
+    "contractors": [
+      { "contractor_name": "Mahesh Rana",
+       "mobile_number": "9000005101",
+        "jobs_pending": 10,
+         "rating": 3 
+      },
+      { "contractor_name": " Pravin Chaudhari",
+       "mobile_number": "9000005102",
+        "jobs_pending": 6, 
+        "rating": 4 
+     },
+      { "contractor_name": "Ashok Patel",
+       "mobile_number": "9000005103",
+        "jobs_pending": 2, 
+        "rating": 5 
+     },
+      { "contractor_name": "Dinesh Makwana",
+       "mobile_number": "9000005104",
+        "jobs_pending": 7, 
+        "rating": 3 
+     },
+      { "contractor_name": "Jignesh Bharwad",
+       "mobile_number": "9000005105",
+        "jobs_pending": 5, 
+        "rating": 4 
+     }
+    ]
+  }
+]
